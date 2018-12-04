4a68dfacd5a2968bdd53a8db75ee7a7ab7669337
BixBite-project <38261741+BixBite-project@users.noreply.github.com>
2018-12-04 21:43:10 +0500

    deamon update

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from cx_Freeze import setup, Executable
 
 setup(
     name = "wallet",
-    version = "0.0.0.1",
+    version = "0.0.0.2",
     description = "BixbiteGUIWallet",
     executables = [Executable("wallet.py")]
 )
+
+
+pyinstaller wallet.py --icon=Resources\icons\bixbite.ico --windowed
+pyinstaller --onefile --windowed --icon=bixbite.ico wallet.py
+
+get-FileHash .\bootstrap.min.css | Format-List
